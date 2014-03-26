c2e3f71aedec5842a187d1ae6976cb823e25bf76
subediramji <ramjisubedi1991@yahoo.com>
2014-03-26 17:13:44 +0545

    work in commit

--- application/views/menuview/allPost.php
+++ application/views/menuview/allPost.php
@@ -15,18 +15,115 @@
                     ?>
                        
     <?php if($data->description==1)
-    {
-     echo 'allowed for all';                   
-    }
+    { 
+        //================ IF ALL ALLOW COMMIT ================//
+        ?>
+                            
+    <div id="fb-root"></div>
+    <script src="//connect.facebook.net/en_US/all.js"></script>
+    
+    <script type="text/javascript">
+      
+      // Initialize the Facebook JavaScript SDK
+      FB.init({
+        appId: '798589833503780',
+        xfbml: true,
+        status: true,
+        cookie: true,
+      });
+      
+      // Check if the current user is logged in and has authorized the app
+      FB.getLoginStatus(checkLoginStatus);
+      
+      // Login in the current user via Facebook and ask for email permission
+      function authUser() {
+        FB.login(checkLoginStatus, {scope:'email'});
+      }
+      
+      // Check the result of the user status and display login button if necessary
+      function checkLoginStatus(response) {
+        if(response && response.status == 'connected') {
+          //alert('User is authorized');
+          
+          // Hide the login button
+          document.getElementById('loginButton').style.display = 'none';
+          
+          
+          // Now Personalize the User Experience
+          console.log('Access Token: ' + response.authResponse.accessToken);
+        } else {
+          alert('User is not authorized');
+          
+          // Display the login button
+          document.getElementById('loginButton').style.display = 'block';
+          document.getElementById('subformDiv').style.display = 'none';
+        }
+      }
+    </script>
+    
+    <input id="loginButton" type="button" value="Login!" onclick="authUser();" />
+    <div id="subformDiv">
+        <?php echo form_open_multipart();?>
+        <textarea name="txtarea"></textarea>
+        <input type="submit" value="Submit" />
+        <?php echo form_close();?>
+    </div>                   
+    <?php }
  else {
      if($post->allow_comment==1)
      {
-         echo 'allowed for this post';
-     }
- else {
-         echo 'for this not allowed ';    
-     }
-     
+         //============== IF ONE POST ALLOW ==========//
+         ?>
+         <div id="fb-root"></div>
+    <script src="//connect.facebook.net/en_US/all.js"></script>
+    
+    <script type="text/javascript">
+      
+      // Initialize the Facebook JavaScript SDK
+      FB.init({
+        appId: '798589833503780',
+        xfbml: true,
+        status: true,
+        cookie: true,
+      });
+      
+      // Check if the current user is logged in and has authorized the app
+      FB.getLoginStatus(checkLoginStatus);
+      
+      // Login in the current user via Facebook and ask for email permission
+      function authUser() {
+        FB.login(checkLoginStatus, {scope:'email'});
+      }
+      
+      // Check the result of the user status and display login button if necessary
+      function checkLoginStatus(response) {
+        if(response && response.status == 'connected') {
+          //alert('User is authorized');
+          
+          // Hide the login button
+          document.getElementById('loginButton').style.display = 'none';
+          
+          
+          // Now Personalize the User Experience
+          console.log('Access Token: ' + response.authResponse.accessToken);
+        } else {
+          alert('User is not authorized');
+          
+          // Display the login button
+          document.getElementById('loginButton').style.display = 'block';
+          document.getElementById('subformDiv').style.display = 'none';
+        }
+      }
+    </script>
+    
+    <input id="loginButton" type="button" value="Login!" onclick="authUser();" />
+    <div id="subformDiv">
+        <?php echo form_open_multipart();?>
+        <textarea name="txtarea"></textarea>
+        <input type="submit" value="Submit" />
+        <?php echo form_close();?>
+    </div>
+     <?php }
        
     }
                         
