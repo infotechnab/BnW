2bb55f428dfdb9977d6990091f6c803e54e9abfc
bhomnath <bhomnath@outlook.com>
2014-04-09 11:57:21 +0545

    template_meta_data created for template

---
+++ application/views/menuview/template_meta_data.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * meta data infromaio for template
+ * 
+ * Template Name: #
+ * Template Author: #
+ * Tempalte Type: Free
+ * Template Created On: #
+ * 
+ */
+
+$navigaton_site=array("Header","Sidebar","Body","Footer" );
+$menu_position=array("Header","Sidebar","Body","Footer");
+
+
+
+?>
