09237bf7f6ffe4edd0067e6f3e908efc6ce220e7
bhomnath <bhomnath@outlook.com>
2015-05-15 13:50:37 +0545

    form altered

--- application/views/default/template/contactForm.php
+++ application/views/default/template/contactForm.php
@@ -1,22 +1,5 @@
- <link rel="stylesheet" href="<?php echo base_url().'content/uploads/styles/form.css'; ?>">
- <script src="<?php echo base_url().'content/uploads/scripts/forms.js'; ?>"></script>
- <style>
-     .alert-success {
-    background-color: #dff0d8;
-    border-color: #d6e9c6;
-    color: #3c763d;
-}
-.alert {
-    border: 1px solid transparent;
-    border-radius: 4px;
-    margin-bottom: 20px;
-    padding: 15px;
-    display: none;
-}
-     
- </style>
- <div id="pattern-3">
- <?php if(!empty($contact)){
+<div id="pattern-3">
+    <?php if(!empty($contact)){
      foreach($contact as $cinfo){
          $name = $cinfo->name;
          $address = $cinfo->address;
@@ -27,121 +10,125 @@
          $map = $cinfo->show_map;
      } 
      ?>
- 
- <div class="container" style="padding-top:50px;min-height: 820px;">
-  <div class="row">
 
-      <?php if($map =="showMap"){ ?>
-      <section class="about mt100">
-      <div class="col-md-6">
-        <h2 class="lined-headingcf bounceInRight appear" data-start="800"><span>Location Map</span></h2>
-        <style>
-      #map_canvas {
-        width: 94%;
-        height: 500px;
-        margin: 0% 3% 0% 3%;
-      }
-    </style>
-    <script src="https://maps.googleapis.com/maps/api/js"></script>
-    <script>
-      function initialize() {
-        var map_canvas = document.getElementById('map_canvas');
-         var myLatlng = new google.maps.LatLng(27.687278, 84.427995);
-        var map_options = {
-          center: myLatlng,
-          zoom: 12,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(map_canvas, map_options);
-        
-        var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h3 id="firstHeading" class="firstHeading" style="color:#000;"> BGR Chitwan</h3>'+
-      '<div id="bodyContent">'+
-      '<p>Bharatpur Height, Bharatpur, Chitwan'+
-      '</p>'+
-      '</div>'+
-      '</div>';
+<div class="container">
 
- var infowindow = new google.maps.InfoWindow({
-      content: contentString
-  });
+<!-- Contact with Map - START -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="well well-sm">
+                <div class="alert alert-success" style="display: none;"></div>
+                  <?php if($form =="showForm"){ ?>
+                 <?php $attributes = array('class' => 'form-horizontal'); echo form_open_multipart('subscribers/addFeedback', $attributes); ?>
+                    <fieldset>
+                        <legend class="text-center header">Contact us</legend>
+                        <div class="form-group">
+                            <div class="col-md-10 col-md-offset-1">
+                                <input id="fname" name="name" type="text" placeholder="Full Name" required class="form-control">
+                            </div>
+                        </div>
+                        
 
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'BGR, Bharatpur)'
-  });
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
-}
+                        <div class="form-group">
+                            <div class="col-md-10 col-md-offset-1">
+                                <input id="email" name="email" type="text" placeholder="Email Address" required class="form-control">
+                            </div>
+                        </div>
 
-google.maps.event.addDomListener(window, 'load', initialize);
+                        <div class="form-group">
+                            <div class="col-md-10 col-md-offset-1">
+                                <input id="phone" name="phone" type="text" placeholder="Phone" required class="form-control">
+                            </div>
+                        </div>
 
-    </script> 
-        <div id="map_canvas"></div>
-      </div>
-      </section>
-      <?php } ?>
-      
-      
-      
-      
-      <section class="about mt100">
-      <div class="col-md-6">
-        <h2 class="lined-headingcf bounceInRight appear" data-start="800"><span>Contact Info</span></h2>
-        <dl>
-                                <dt><p><?php echo $name; ?><br>
-                                    <?php echo $address; ?><br>
-                                </dt>
-                                <dd><span>Phone: </span><?php echo $contact1; ?>, <?php echo $contact2; ?></dd>
-                                <dd><span>Email: </span><?php echo $email; ?></dd>
-                            </dl>
-        <?php if($form =="showForm"){ ?>
-        <h2 class="lined-headingcf bounceInRight appear" data-start="800"><span>Contact Form</span></h2>
-         <div class="alert alert-success">
+                        <div class="form-group">
+                            <div class="col-md-10 col-md-offset-1">
+                                <textarea class="form-control" id="message" name="message" required placeholder="Enter your massage for us here. We will get back to you within 2 business days." rows="7"></textarea>
+                            </div>
+                        </div>
 
-        <a href="#" class="close" data-dismiss="alert">&times;</a>  
-
+                        <div class="form-group">
+                            <div class="col-md-12 text-center">
+                                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+                  <?php } ?>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div>
+                <div class="panel panel-default">
+                    <div class="text-center header">Our Office</div>
+                    <div class="panel-body text-center">
+                        <h4><?php echo $name; ?></h4>
+                       
+                        <div>
+                       <i class="fa fa-map-marker"></i> <?php echo $address; ?><br/>
+                      <?php if(!empty($contact1)){ ?> <i class="fa fa-phone"></i> <?php echo $contact1; ?><br/> <?php } ?>
+                        <?php if(!empty($contact2)){ ?> <i class="fa fa-skype"></i> <?php echo $contact2; ?><br/> <?php } ?>
+                        <i class="fa fa-envelope"></i> <?php echo $email; ?>
+                        </div>
+                         <?php if($map =="showMap"){ ?>
+                        <hr />
+                        <div id="map1" class="map">
+                        </div>
+                         <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-        
-        <?php $attributes = array('id' => 'form'); echo form_open_multipart('subscribers/addFeedback', $attributes); ?>
+</div>
 
-       <div class="success_wrapper">
-       <div class="success">Contact form submitted!<br>
-       <strong>We will be in touch soon.</strong> </div></div>
-       <fieldset>
-       <label class="name">
-       <input type="text" name="name" value="Name">
-       <br class="clearfix">
-       <span class="error error-empty">*This is not a valid name.</span><span class="empty error-empty">*This field is required.</span> </label>
-       <label class="email">
-       <input type="text" name="email" value="E-mail">
-       <br class="clearfix">
-       <span class="error error-empty">*This is not a valid email address.</span><span class="empty error-empty">*This field is required.</span> </label>
-       <label class="phone">
-       <input type="tel" name="phone" value="Phone">
-       <br class="clearfix">
-       <span class="error error-empty">*This is not a valid phone number.</span><span class="empty error-empty">*This field is required.</span> </label>
-       <label class="message">
-           <textarea name="message" placeholder="Message" style="color:ccc;"></textarea>
-       <br class="clearfix">
-       <span class="error">*The message is too short.</span> <span class="empty">*This field is required.</span> </label>
-       <div class="clearfix"></div>
-       <a data-type="reset" class="btn">Clear</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a data-type="submit" class="btn">Send</a>
-       <div class="clearfix"></div>
-       </fieldset><?php echo form_close(); ?>
-        
-        
-        <?php } ?>
-      </div>
-      </section>
- 
-  </div>
- </div>
- <?php }else {
-         $this->load->view('gardenResort/errorPage');
+
+<script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+
+<script type="text/javascript">
+    jQuery(function ($) {
+        function init_map1() {
+            var myLocation = new google.maps.LatLng(27.691062, 84.426885);
+            var mapOptions = {
+                center: myLocation,
+                zoom: 15
+            };
+            var marker = new google.maps.Marker({
+                position: myLocation,
+                title: "Property Location"
+            });
+            var map = new google.maps.Map(document.getElementById("map1"),
+                mapOptions);
+            marker.setMap(map);
+        }
+        init_map1();
+    });
+</script>
+
+<style>
+    .map {
+        min-width: 300px;
+        min-height: 300px;
+        width: 100%;
+        height: 100%;
+    }
+
+    .header {
+        background-color: #F5F5F5;
+        color: #36A0FF;
+        height: 70px;
+        font-size: 27px;
+        padding: 10px;
+    }
+</style>
+
+<!-- Contact with Map - END -->
+
+</div>
+
+<?php }else {
+         $this->load->view('default/template/errorPage');
     } ?>
- </div>     
+</div>
+
