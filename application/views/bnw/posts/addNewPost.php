a390d0346f5a2541b80c8bc2f3cb0ef17a51d3e6
bhomnath <bhomnath@outlook.com>
2015-05-28 14:18:36 +0545

    image crop

--- application/views/bnw/posts/addNewPost.php
+++ application/views/bnw/posts/addNewPost.php
@@ -1,129 +1,172 @@
-<link rel="stylesheet" type="text/css" href="<?php echo base_url().'content/bnw/styles/imgareaselect-animated.css'; ?>" />
- <script type="text/javascript" src="<?php echo base_url().'content/bnw/scripts/jquery.imgareaselect.pack.js'; ?>"></script>
-	<script type="text/javascript" src="<?php echo base_url().'content/bnw/scripts/script.js'; ?>"></script>
-        
-        <div class="rightSide">
-    <?php 
-if ($query)
-    
-{
-    $i=0;
-    foreach ($query as $data)
-    {        
-       $set_data[$i] = $data->description;
-       $i++;      
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'content/bnw/styles/imgareaselect-animated.css'; ?>" />
+<script type="text/javascript" src="<?php echo base_url() . 'content/bnw/scripts/jquery.imgareaselect.pack.js'; ?>"></script>
+<script type="text/javascript" src="<?php echo base_url() . 'content/bnw/scripts/script.js'; ?>"></script>
+
+<div class="rightSide">
+    <?php
+    if ($query) {
+        $i = 0;
+        foreach ($query as $data) {
+            $set_data[$i] = $data->description;
+            $i++;
+        }
     }
- }
- ?>
+    ?>
     <div class="titleArea">
-     <h2>Add new Post&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo base_url().'index.php/offers/posts'; ?>">View All</a></h2>
-<hr class="hr-gradient"/>  
-<div id="sucessmsg">
-  <?php if($this->session->flashdata('message')){echo $this->session->flashdata('message');}?>
-      <?php echo validation_errors();
-  if(isset($error))
-  {
-      echo $error;
-  }
-  ?>
-</div>
+        <h2>Add new Post&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo base_url() . 'index.php/offers/posts'; ?>">View All</a></h2>
+        <hr class="hr-gradient"/>  
+        <div id="sucessmsg">
+<?php if ($this->session->flashdata('message')) {
+    echo $this->session->flashdata('message');
+} ?>
+            <?php
+            echo validation_errors();
+            if (isset($error)) {
+                echo $error;
+            }
+            ?>
+        </div>
     </div>
-    
+
     <div id="forLeftPage">
 
-  
 
-  <?php echo form_open_multipart('offers/addpost');?>
-  
-  <p class="dashuppe-text-all">Post Title<br />
-  <input type="text" class="textInput" name="post_title" value="<?php echo set_value('post_title'); ?>" />
-  </p>
-  <p class="dashuppe-text-all">Body<br />
-      <textarea name="post_content" id="textara" cols="50" rows="5" ><?php echo set_value('post_content'); ?></textarea>
-  
-  </p> 
-  <p class="dashuppe-text-all">Image<br />
-     <input id="uploadImage" class="textInput" type="file" name="file" accept="image/*"/>
-     <!-- hidden inputs -->
-		<input type="hidden" id="x" name="x" />
-		<input type="hidden" id="y" name="y" />
-		<input type="hidden" id="w" name="w" />
-		<input type="hidden" id="h" name="h" /> 
-  </p> 
-   <img id="uploadPreview" style="display:none;width:1000px;"/>
-  <p class="dashuppe-text-all">Select Category<br/>
-       <select class="textInput" name="selectCategory">
-           <option value="0">Select None</option>
+
+        <?php echo form_open_multipart('offers/addpost'); ?>
+
+        <p class="dashuppe-text-all">Post Title<br />
+            <input type="text" class="textInput" name="post_title" value="<?php echo set_value('post_title'); ?>" />
+        </p>
+        <p class="dashuppe-text-all">Body<br />
+            <textarea name="post_content" id="textara" cols="50" rows="5" ><?php echo set_value('post_content'); ?></textarea>
+
+        </p> 
+        <p class="dashuppe-text-all">Image<br />
+            <input id="uploadImage" class="textInput" type="file" name="file" accept="image/*"/>
+            <!-- hidden inputs -->
+            <input type="hidden" id="x" name="x" />
+            <input type="hidden" id="y" name="y" />
+            <input type="hidden" id="w" name="w" />
+            <input type="hidden" id="h" name="h" /> 
+        </p> 
+        <div data-toggle="buttons" id="mybutton" class="btn-group btn-group-justified">
+            <label title="Set Aspect Ratio" data-option="1.7777777777777777" data-method="setAspectRatio" class="btn btn-primary active">
+                <input type="radio" value="1.7777777777777777" name="aspestRatio" id="aspestRatio1" class="sr-only">16:9
+            </label>
+            <label title="Set Aspect Ratio" data-option="1.3333333333333333" data-method="setAspectRatio" class="btn btn-primary">
+                <input type="radio" value="1.3333333333333333" name="aspestRatio" id="aspestRatio2" class="sr-only" checked="checked">4:3
+            </label>
+            <label title="Set Aspect Ratio" data-option="1" data-method="setAspectRatio" class="btn btn-primary">
+                <input type="radio" value="1" name="aspestRatio" id="aspestRatio3" class="sr-only">1:1
+            </label>
+            <label title="Set Aspect Ratio" data-option="0.6666666666666666" data-method="setAspectRatio" class="btn btn-primary">
+                <input type="radio" value="0.6666666666666666" name="aspestRatio" id="aspestRatio4" class="sr-only">2:3
+            </label>
+            <label title="Set Aspect Ratio" data-option="NaN" data-method="setAspectRatio" class="btn btn-primary">
+                <input type="radio" value="NaN" name="aspestRatio" id="aspestRatio5" class="sr-only">Free
+            </label>
+        </div>
+        <img id="uploadPreview" style="display:none;width:1000px;"/>
+        <p class="dashuppe-text-all">Select Category<br/>
+            <select class="textInput" name="selectCategory">
+                <option value="0">Select None</option>
                 <?php
-                foreach ($listOfCategory as $data)
-                {
+                foreach ($listOfCategory as $data) {
                     ?>
-           
-                <option value="<?php echo $data->id; ?>">
-                    <?php echo $data->category_name; ?>
-                </option>
-                    <?php
-                }
-                ?> 
-          
+
+                    <option value="<?php echo $data->id; ?>">
+    <?php echo $data->category_name; ?>
+                    </option>
+                        <?php
+                    }
+                    ?> 
+
             </select>
-       
-   </p>
-   <input type="submit" class="btn btn-primary btn-lg" value="Submit" />
-  <?php echo form_close();?>
+
+        </p>
+        <input type="submit" class="btn btn-primary btn-lg" value="Submit" />
+<?php echo form_close(); ?>
     </div>
-    
+
     <div id="forRightPage"> 
-    
- 
-   
- <!--   <input type="checkbox" name="allow_comment" value="1" <?php //if($set_data[0]==1) echo 'checked' ;?> >Allow people to post comment</input>
+
+
+
+<!--   <input type="checkbox" name="allow_comment" value="1" <?php //if($set_data[0]==1) echo 'checked' ; ?> >Allow people to post comment</input>
 <br/>
-<input type="checkbox" name="allow_like" value="1" <?php // if($set_data[1]==1) echo 'checked' ;?> >Allow people to like </input>
+<input type="checkbox" name="allow_like" value="1" <?php // if($set_data[1]==1) echo 'checked' ; ?> >Allow people to like </input>
 <br/>
-<input type="checkbox" name="allow_share" value="1" <?php //if($set_data[2]==1) echo 'checked' ;?> >Allow people to share</input>
+<input type="checkbox" name="allow_share" value="1" <?php //if($set_data[2]==1) echo 'checked' ; ?> >Allow people to share</input>
 <br/> -->
-  
- 
-  </div>
+
+
+    </div>
 </div>
 <div class="clear"></div>
 </div>
-
+<style>
+    .btn-group {
+    margin-bottom: 5px;
+    margin-top: 5px;
+}
+#mybutton > .btn-primary {
+    background-color: #337ab7;
+    border-color: #2e6da4;
+    color: #fff;
+    width: 60px;
+}
+button, input, select, textarea {
+    font-family: inherit;
+    font-size: inherit;
+    line-height: inherit;
+}
+button, html input[type="button"], input[type="reset"], input[type="submit"] {
+    cursor: pointer;
+}
+button, select {
+    text-transform: none;
+}
+button {
+    overflow: visible;
+}
+    
+    
+    
+</style>
 <!--
   <p>Post Status:<br />
-  <?php 
- /* $options = array(
-                  '1'  => 'publish',
-                  '0'    => 'draft');
+<?php /* $options = array(
+  '1'  => 'publish',
+  '0'    => 'draft');
   echo form_dropdown('post_status',$options,'1')
   ?>
   </p>
-   <p>Post Comment Status:<br />
-  <?php 
+  <p>Post Comment Status:<br />
+  <?php
   $options = array(
-                '2'=> 'as defined',
-                  '1'  => 'public',
-                  '0'    => 'custom');
+  '2'=> 'as defined',
+  '1'  => 'public',
+  '0'    => 'custom');
   echo form_dropdown('comment_status',$options,'1')
   ?>
-  </p>   
-   <p> Post Tags : <br/>
-       <input type="text" name="post_tags" /> </p>
-   
-   <p>Select Category:<br/>
-       <select name="selectCategory">
-                <?php
-                foreach ($listOfCategory as $data)
-                {
-                    ?>
-                <option value="<?php echo $data->category_name; ?>">
-                    <?php echo $data->category_name; ?>
-                </option>
-                    <?php
-                }
-                ?> 
-          
-            </select>
-       
-   </p>--> */ ?>
+  </p>
+  <p> Post Tags : <br/>
+  <input type="text" name="post_tags" /> </p>
+
+  <p>Select Category:<br/>
+  <select name="selectCategory">
+  <?php
+  foreach ($listOfCategory as $data)
+  {
+  ?>
+  <option value="<?php echo $data->category_name; ?>">
+  <?php echo $data->category_name; ?>
+  </option>
+  <?php
+  }
+  ?>
+
+  </select>
+
+  </p>--> */ ?>
+
--- content/bnw/scripts/script.js
+++ content/bnw/scripts/script.js
@@ -8,24 +8,89 @@
  */
 
 // set info for cropping image using hidden fields
-function setInfo(x1c,y1c,x2c,y2c) {
-   var w= x2c - x1c;
-   var h= y2c - y1c;
-	$('#x').val(x1c);
-	$('#y').val(y1c);
-	$('#w').val(w);
-	$('#h').val(h);
+function setInfo(x1c, y1c, x2c, y2c) {
+    var w = x2c - x1c;
+    var h = y2c - y1c;
+    $('#x').val(x1c);
+    $('#y').val(y1c);
+    $('#w').val(w);
+    $('#h').val(h);
 }
 function setVal(i, e) {
-	$('#x').val(e.x1);
-	$('#y').val(e.y1);
-	$('#w').val(e.width);
-	$('#h').val(e.height);
+    $('#x').val(e.x1);
+    $('#y').val(e.y1);
+    $('#w').val(e.width);
+    $('#h').val(e.height);
 }
 
+function abc(newas, asp) {
+
+    var p = $("#uploadPreview");
+    // var asp = $('input[name=aspestRatio]:checked').val()
+    // fadeOut or hide preview
+    p.fadeOut();
+    // prepare HTML5 FileReader
+    var oFReader = new FileReader();
+    oFReader.readAsDataURL(document.getElementById("uploadImage").files[0]);
+    oFReader.onload = function(oFREvent) {
+
+        p.attr('src', oFREvent.target.result).fadeIn();
+        var w = $('img#uploadPreview').width();
+        var h = $('img#uploadPreview').height();
+        if (w > h) {
+            var centre = w / 2;
+            var height = h;
+            var width = height * asp;
+            var deduct = width / 2;
+            var x1c = centre - deduct;
+            var y1c = 0;
+            var x2c = centre + deduct;
+            var y2c = height;
+            var coords = {x1: x1c, y1: y1c, x2: x2c, y2: y2c};
+            onSelectEnd: setVal;
+        } else {
+            var centre = h / 2;
+            var nwidth = w;
+            var nheight = nwidth * asp;
+            var ndeduct = nheight / 2;
+            var x1c = 0;
+            var y1c = centre - ndeduct;
+            var x2c = nwidth;
+            var y2c = centre + ndeduct;
+            var coords = {x1: x1c, y1: y1c, x2: x2c, y2: y2c};
+            onSelectEnd: setVal;
+        }
+
+
+        $('img#uploadPreview').imgAreaSelect({x1: x1c, y1: y1c, x2: x2c, y2: y2c, aspectRatio: newas, handles: true, onload: setInfo(x1c, y1c, x2c, y2c)});
+        //$('img#uploadPreview').imgAreaSelect({aspectRatio:alert(asp),handles: true,onSelectEnd: setVal});
+    };
+}
 
 $(document).ready(function() { 
-    
+     var asp;
+    asp = $('input[name=aspestRatio]:checked').val();
+    $('.sr-only').on('change', function() {
+        asp = $(this).val();
+        var newas;
+        if (asp == "1.7777777777777777") {
+            newas = "16:9";
+        }
+        if (asp == "1.3333333333333333") {
+            newas = "4:3";
+        }
+        if (asp == "1") {
+            newas = "1:1";
+        }
+        if (asp == "0.6666666666666666") {
+            newas = "2:3";
+        }
+        if (asp == "NaN") {
+            newas = "NaN";
+        }
+        abc(newas, asp);
+    });
+
 	var p = $("#uploadPreview");
 	// prepare instant preview
 	$("#uploadImage").change(function(){    
