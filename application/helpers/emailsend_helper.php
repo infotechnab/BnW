1d466a5e5e002644ba0c0f3ffcf6f4b4e7a1ab08
sshrestha@salyani.com.np <shrestha123>
2015-06-14 16:14:23 +0545

    user credential email

--- application/helpers/emailsend_helper.php
+++ application/helpers/emailsend_helper.php
@@ -2,7 +2,56 @@
 
 function send_user_credentials($name, $fname, $lname, $email, $pass, $status)
 {
+     $body = '<div style="width: 750px; margin: 0 auto 0 auto; padding: 0px;font-family: calibri;border: 1px solid #000;" >
+        <div style="text-align:center; min-height: 70px; background-color: #222; margin: 0 auto 0 auto;width: 100%;">
+            <div style="margin: 0 auto;text-align: center;">
+            <img src="'.$imglink.'" style="width:70px; margin:10px;vertical-align:middle;"/>
+            <span style="color: #f8a54d;font-size: 40px;font-family: Trebuchet MS;font-weight:900;vertical-align:middle;">BnW - A Complete CMS</span>
+            </div>
+            </div>
+
+   <div style="padding: 10px 20px 10px 20px;background-color: #fff;">
+   
     
+    <h4>Dear ' . $userName . '  !</h4>
+
+    <h3>Your login credentials are given below.</h3>
+    <p>
+    <table>
+    <tr>
+    <th>Username</th>
+    <td>'.$name.'</td>
+    </tr>
+    <tr>
+    <th>Password</th>
+    <td>'.$pass.'</td>
+    </tr>
+    </table>
+    <p>
+    <p>You can use these <b>usename</b> and <b>password</b> to login to your account.</p>
+                <br/><br/><br/>   
+<h5>Thank You</h5>
+<h5>BnW Team</h5>
+</div>
+            
+           <div style="text-align:center; height: 40px;padding-top: 10px;">
+           <p>&copy; BnW</p>
+
+            </div>
+
+</div>';
+   
+    $headers = "From: BnW <info@salyani.com.np>" . "\r\n";
+    $headers .= "Reply-To: ".$email. "\r\n";
+    $headers .="MIME-Version: 1.0" . "\r\n";
+    $headers .="Content-type:text/html;charset=UTF-8" . "\r\n";          
+    $subject = "User login Credentials";
+    if (mail($email, $subject, $body, $headers)) {
+       $this->session->set_flashdata("message","User credentials has been emailed to the user $email");
+    } else {
+        $this->session->set_flashdata("message","Failed to email user cresentials to the user $email");
+         } 
+     
 }
     function contact_email_hotel($name, $email, $phone, $imglink, $htitle, $messagess)
 {
