042ea1d53b61b55196be2f43e553671df69e47ab
sshrestha@salyani.com.np <shrestha123>
2015-06-14 16:41:18 +0545

    table style

--- application/views/bnw/posts/postListingCategory.php
+++ application/views/bnw/posts/postListingCategory.php
@@ -25,7 +25,7 @@
     
          if(!empty($query)){
              ?>
-        <table border="1" cellpadding="10">
+        <table border="1" cellpadding="10" class="tbl_full_width" >
         <tr>
             
             <th>Post Title</th>
