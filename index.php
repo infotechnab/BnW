a62d76dab409f79e31668f05c90b2299a7d98074
bhomnath <bhomnath@salyani.com.np>
2016-05-08 15:21:52 +0545

    index file modified

--- index.php
+++ index.php
@@ -297,14 +297,14 @@ require_once BASEPATH.'core/CodeIgniter.php';
 
         $back=current_url();
         $lower_back=strtolower($back);
-        var_dump($lower_back);
-    if(!isset($CI->session->userdata["admin_logged_in"]) || $CI->session->userdata["admin_logged_in"]!=true){
-            $url_access_array=array("bnw","album","contact","dashboard","demo","error","events","gadgets","imagemanipulator","media","offers","publication","setting","sliders","social_share","subscribers","user","viewcaptcha"); //not access
+       
+//    if(!isset($CI->session->userdata["admin_logged_in"]) || $CI->session->userdata["admin_logged_in"]!=true){
+ //           $url_access_array=array("bnw","album","contact","dashboard","demo","error","events","gadgets","imagemanipulator","media","offers","publication","setting","sliders","social_share","subscribers","user","viewcaptcha"); //not access
                 
-            var_dump((has_in_url($lower_back, $url_access_array)));{
-                     $CI->session->set_flashdata('message', 'Unauthorized access attempted');
-                    if(has_redirect($back)){
-                       if(has_redirect_to_login($back)){
+ //           if(has_in_url($lower_back, $url_access_array));{
+ //                    $CI->session->set_flashdata('message', 'Unauthorized access attempted');
+ //                   if(has_redirect($back)){
+ //                      if(has_redirect_to_login($back)){
 //                           ob_end_clean();
 //                            header("location:".base_url()."login/index/");
 //                       }else{
@@ -319,10 +319,10 @@ require_once BASEPATH.'core/CodeIgniter.php';
 //                            }else{
 //                                ob_end_clean();
 //                                 redirect('login/index/?url=' . $back, 'refresh');
-                            }
-                   }
-               }
-      }
+//                            }
+//                   }
+ //              }
+ //     }
 
     
     
