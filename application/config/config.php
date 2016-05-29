83783dd7ac8693b25a7d058651c398f55459f88e
jonasahtserhs <jonasahtserhs@gmail.com>
2016-05-29 14:14:52 +0545

    config file changed

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/BnWold/';
+$config['base_url'] = 'http://localhost/Bnw/';
 
 /*
 |--------------------------------------------------------------------------
